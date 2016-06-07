6bf3e3e3548f9421142f3a895b3aea63bdc9dcec
Tamás <Tams@TOMLAPTOP>
2016-06-08 00:43:58 +0200

    tested board, move gen

--- PuzzleCreatorConsole/PuzzleCreatorConsole/Program.cs
+++ PuzzleCreatorConsole/PuzzleCreatorConsole/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using PgnFileTools;
 using System.IO;
+using ZhTools;
 
 namespace PuzzleCreatorConsole
 {
@@ -12,14 +13,23 @@ namespace PuzzleCreatorConsole
     {
         static void Main(string[] args)
         {
-            var pgnReader = new PgnStreamReader();
-            var reader = File.OpenText("lichess1.pgn");
-            var gameInfos = pgnReader.ReadToList(reader);
-            //var zh = gameInfos.Where(gi => gi.Headers["Variant"] == "Crazyhouse").ToList();
-            var err = gameInfos.Where(gi => gi.Headers.Count==0).ToList();
-            var list = gameInfos.ToList<GameInfo>();
-            var list2 = list;
-            var m = gameInfos[0].Moves.Select(mv => mv.ToAlgebraicString()).ToList();           
+            /* 
+             var pgnReader = new PgnStreamReader();
+             var reader = File.OpenText("lichess1.pgn");
+             var gameInfos = pgnReader.ReadToList(reader);
+             //var zh = gameInfos.Where(gi => gi.Headers["Variant"] == "Crazyhouse").ToList();
+             var err = gameInfos.Where(gi => gi.Headers.Count==0).ToList();
+             var list = gameInfos.ToList<GameInfo>();
+             var list2 = list;
+             var m = gameInfos[0].Moves.Select(mv => mv.ToAlgebraicString()).ToList();           
+             */
+
+            var b = new Board();
+            int x;
+            var h = b.GenerateMoves(out x);
+            b.MakeUserMove("e2e4");
+            b.MakeUserMove("d7d5");
+            var h2 = b.GenerateMoves(out x);
         }
     }
 }
